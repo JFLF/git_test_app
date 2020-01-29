e30e6d42e425be4a6b47bfb60644e1a4d006f35b
puvsik <puvsik@gmail.com>
2020-01-29 23:22:32 +0300

    Adding files

---
+++ git_test_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git_test_app
+//
+//  Created by Daniel on 29/01/2020.
+//  Copyright © 2020 KurachkinSiarhei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
