5ad43d8727925b5ac57accf43a27a34128c1fa4a
puvsik <puvsik@gmail.com>
2020-01-31 20:52:53 +0300

    Add print

--- git_test_app/ViewController.swift
+++ git_test_app/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("show must go on")
+        print("... and on")
     }
 
 
